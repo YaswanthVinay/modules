25388461248ec98771608824cfc27bdf37dcc279
Yaswanth Vinay <97665352+YaswanthVinay@users.noreply.github.com>
2024-04-17 16:22:09 +0530

    Add files via upload
    main files

---
+++ terr-modules/variables.tf
@@ -0,0 +1,8 @@
+variable "vpc_cidr" {}
+variable "proj_name" {}
+variable "app-sn-cidr" {}
+variable "azs" {}
+variable "db-sn-cidr" {}
+variable "web-sn-cidr" {}
+variable "igw_cidr"{}
+variable "instance_type"{}
